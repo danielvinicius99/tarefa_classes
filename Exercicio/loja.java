9124830d84f977d335a0e3ba304b4c54465b5590
Daniel Vinicius <danielvinicius100@hotmail.com>
2024-01-19 14:20:03 -0300

    commit inicial

---
+++ Exercicio/loja.java
@@ -0,0 +1,36 @@
+public class loja {
+
+    private int codigoId;
+    private String nome;
+    private String endereço;
+
+    public int getCodigoId() {
+        return codigoId;
+    }
+
+    public void setCodigoId(int codigoId) {
+        this.codigoId = codigoId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+
+    /**
+     * Este é o comentário do código.
+     */
+
+}
